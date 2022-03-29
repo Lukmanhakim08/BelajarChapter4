3074c568c0878778888d98ed69f4a34d0874e4c4
lukmanhakim08 <lukman081217@gmail.com>
2022-03-29 23:24:07 +0700

    Blom kelar lanjut bsok

--- app/src/main/java/com/example/belajarchapter4/PertemuanKedua/LatihanKedua/KasirActivity.kt
+++ app/src/main/java/com/example/belajarchapter4/PertemuanKedua/LatihanKedua/KasirActivity.kt
@@ -9,6 +9,7 @@ import com.example.belajarchapter4.PertemuanKedua.alertFragment.FragmentFragment
 import com.example.belajarchapter4.R
 import kotlinx.android.synthetic.main.activity_alert_dialog.*
 import kotlinx.android.synthetic.main.activity_kasir.*
+import kotlinx.android.synthetic.main.custom_kasir.*
 import kotlinx.android.synthetic.main.custom_kasir.view.*
 import kotlinx.android.synthetic.main.custum_dialog.view.*
 
@@ -18,10 +19,11 @@ class KasirActivity : AppCompatActivity() {
         setContentView(R.layout.activity_kasir)
 
         btn_custemlayout.setOnClickListener {
-//            val nmbarang = edt_nmbarang.text.toString()
-//            val jmbarang = edt_jumlah.text.toString().toInt()
-//            val hrgbarang = edt_hargabrng.text.toString().toInt()
-//            val total = jmbarang * hrgbarang
+
+            val nmbarang = edt_nmbarang.text.toString()
+            val jmbarang = edt_jumlah.text.toString().toInt()
+            val hrgbarang = edt_hargabrng.text.toString().toInt()
+            val total = jmbarang * hrgbarang
             val custom = LayoutInflater.from(this).inflate(R.layout.custom_kasir, null, false)
             val ab = AlertDialog.Builder(this)
                 .setView(custom)
@@ -29,14 +31,15 @@ class KasirActivity : AppCompatActivity() {
 
             custom.btn_bayar.setOnClickListener {
                 val bayar = custom.edt_bayar.text.toString().toInt()
-
+                txt_total.append("$total")
+                val kembalian = total - bayar
+                ab.dismiss()
             }
-//            custom.btn_cusomalert.setOnClickListener {
-////                val name = custom.edt_namaku.text.toString()
-//                Toast.makeText(this, "Nama", Toast.LENGTH_LONG).show()
-//                ab.dismiss()
-//            }
             ab.show()
+            text_pesan.text = nmbarang +
+                    jmbarang +
+                    hrgbarang +
+                    total
         }
 
         btn_custemfragment.setOnClickListener {
