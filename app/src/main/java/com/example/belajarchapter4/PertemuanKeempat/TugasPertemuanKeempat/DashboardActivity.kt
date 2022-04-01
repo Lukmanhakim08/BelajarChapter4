88cf82635c496f7c9d318c440587c0628219daeb
Lukman Hakim <lukman081217@gmail.com>
2022-04-02 01:00:40 +0700

    Tugas Pertemuan Keempat Selesai

--- app/src/main/java/com/example/belajarchapter4/PertemuanKeempat/TugasPertemuanKeempat/DashboardActivity.kt
+++ app/src/main/java/com/example/belajarchapter4/PertemuanKeempat/TugasPertemuanKeempat/DashboardActivity.kt
@@ -1,12 +1,50 @@
 package com.example.belajarchapter4.PertemuanKeempat.TugasPertemuanKeempat
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.belajarchapter4.PertemuanKeempat.MainSFActivity
 import com.example.belajarchapter4.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_main_sf2.*
+import kotlinx.android.synthetic.main.custum_dialog.view.*
+import kotlinx.android.synthetic.main.logout_layout.view.*
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var sf : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        //Initialen isi dari variabel global
+        sf = getSharedPreferences("login", Context.MODE_PRIVATE)
+        val getusername = sf.getString("USERNAME", "")
+        text_username.setText(" Hallo, $getusername")
+
+        btn_keluar.setOnClickListener {
+            val custumdialog = LayoutInflater.from(this).inflate(R.layout.logout_layout, null, false)
+            val ab = AlertDialog.Builder(this)
+                ab.setTitle("Apakah anda yakin logout ?")
+                .setCancelable(false)
+                .setView(custumdialog)
+                .create()
+            custumdialog.btn_ya.setOnClickListener {
+                val logoutsf = sf.edit()
+                logoutsf.clear()
+                logoutsf.apply()
+                startActivity(Intent(this, HalLoginActivity::class.java))
+                Toast.makeText(this, "Berhasil Logout", Toast.LENGTH_LONG).show()
+                finish()
+            }
+            custumdialog.btn_tidak.setOnClickListener {
+                startActivity(Intent(this, DashboardActivity::class.java))
+                Toast.makeText(this, "Tidak jadi logout", Toast.LENGTH_LONG).show()
+            }
+            ab.show()
+        }
     }
 }
--- app/src/main/java/com/example/belajarchapter4/PertemuanKeempat/TugasPertemuanKeempat/HalLoginActivity.kt
+++ app/src/main/java/com/example/belajarchapter4/PertemuanKeempat/TugasPertemuanKeempat/HalLoginActivity.kt
@@ -1,12 +1,46 @@
 package com.example.belajarchapter4.PertemuanKeempat.TugasPertemuanKeempat
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.belajarchapter4.PertemuanKeempat.MainSF2Activity
 import com.example.belajarchapter4.R
+import kotlinx.android.synthetic.main.activity_hal_login.*
+import kotlinx.android.synthetic.main.activity_main_sfactivity.*
 
 class HalLoginActivity : AppCompatActivity() {
+    //membuat variabel globa
+    lateinit var prefs : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hal_login)
+
+        if(getSharedPreferences("login", Context.MODE_PRIVATE).contains("USERNAME")){
+            startActivity(Intent(this, DashboardActivity::class.java))
+            finish()
+        }
+        //Initialen isi dari variabel global
+        prefs = getSharedPreferences("login", Context.MODE_PRIVATE)
+
+        btn_login.setOnClickListener {
+            //langkah memasukkan data ke sharePrences
+            val username = edt_username.text.toString()
+            val password = edt_password.text.toString()
+
+            if(username.isEmpty()|| password.isEmpty()){
+                Toast.makeText(this, "username dan password salah", Toast.LENGTH_SHORT).show()
+            }
+            if(username == "lukman"|| password == "admin"){
+                val sv = prefs.edit()
+                sv.putString("USERNAME", username)
+                sv.apply()
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+                Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
